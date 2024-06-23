c7e37d4cb54d7dd8cbffa587ca8a19c5de78c9d7
Christian M <chriamue@gmail.com>
2024-06-23 06:43:02 +0200

    :sparkles: use introjs styles

--- examples/basic.rs
+++ examples/basic.rs
@@ -5,24 +5,25 @@ use yew_tou_rs::{Tour, TourStep};
 pub fn app() -> Html {
     let steps = vec![
         TourStep {
-            selector: "h1".to_string(),
-            text: "This is a title".to_string(),
+            selector: ".h1-step".to_string(),
+            content: "This is a title".to_string(),
         },
         TourStep {
-            selector: "p".to_string(),
-            text: "This is a paragraph".to_string(),
+            selector: ".p-step".to_string(),
+            content: "This is a paragraph".to_string(),
         },
         TourStep {
-            selector: "button".to_string(),
-            text: "This is a button".to_string(),
+            selector: ".button-step".to_string(),
+            content: "This is a button".to_string(),
         }
     ];
 
     html! {
         <>
-            <h1>{"Hello, Yew Tou-rs"}</h1>
-            <p>{"This is a simple example of a Yew Tou-rs."}</p>
-            <Tour steps={steps} />
+            <h1 class="h1-step" data-title="h1" data-intro="This is a title">{"Hello, Yew Tou-rs"}</h1>
+            <p class="p-step" data-title="p" data-intro="This is a paragraph">{"This is a simple example of a Yew Tou-rs."}</p>
+            <button class="button-step">{"Click me"}</button>
+            <Tour steps={steps} selector={"introduction"}/>
         </>
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,82 +1,133 @@
-use yew::prelude::*;
-use web_sys::Element;
 use wasm_bindgen::JsCast;
+use web_sys::Element;
+use yew::prelude::*;
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct TourStep {
     pub selector: String,
-    pub text: String,
+    pub content: String,
 }
 
 #[derive(Properties, Clone, Debug, PartialEq)]
 pub struct TourConfig {
+    pub selector: String,
     pub steps: Vec<TourStep>,
 }
+
 #[function_component(Tour)]
 pub fn tour(config: &TourConfig) -> Html {
     let current_step = use_state(|| 0usize);
+    let last_selector = use_state(|| "".to_string());
 
     let highlight_element = {
         let current_step = current_step.clone();
+        let last_selector = last_selector.clone();
         let steps = config.steps.clone();
+        let selector = config.selector.clone();
         Callback::from(move |_| {
             let current_selector = &steps[*current_step].selector;
+            clear_highlight(&*last_selector);
             if let Some(element) = web_sys::window()
                 .unwrap()
                 .document()
                 .unwrap()
-                .query_selector(current_selector)
+                .query_selector(&current_selector)
                 .unwrap()
             {
-                clear_highlight();
-                element.set_attribute("style", "border: 2px solid red;").unwrap();
+                element
+                    .set_attribute("class", "introjs-helperNumberLayer")
+                    .unwrap();
+                element
+                    .set_attribute("style", "border: 2px solid red;")
+                    .unwrap();
             }
         })
     };
 
     let on_next = {
+        let config = config.clone();
         let current_step = current_step.clone();
+        let last_selector = last_selector.clone();
         let step_count = config.steps.len();
         let highlight_element = highlight_element.clone();
         Callback::from(move |_| {
             if *current_step < step_count - 1 {
+                last_selector.set(config.steps[*current_step].selector.clone());
                 current_step.set(*current_step + 1);
-
                 highlight_element.emit(());
             }
         })
     };
 
     let on_prev = {
+        let config = config.clone();
         let current_step = current_step.clone();
+        let last_selector = last_selector.clone();
         let highlight_element = highlight_element.clone();
         Callback::from(move |_| {
             if *current_step > 0 {
+                last_selector.set(config.steps[*current_step].selector.clone());
                 current_step.set(*current_step - 1);
-
                 highlight_element.emit(());
             }
         })
     };
 
     html! {
-        <div>
-            <div>{"Step: "}{*current_step}</div>
-            <div>{"Selector: "}{&config.steps[*current_step].selector}</div>
-            <div>{"Text: "}{&config.steps[*current_step].text}</div>
-            <button onclick={on_prev}>{"Prev"}</button>
-            <button onclick={on_next}>{"Next"}</button>
-        </div>
+        <>
+            <div class="introjs-overlay"></div>
+            <div class="introjs-helperLayer">
+                <div class="introjs-tooltipReferenceLayer"></div>
+            </div>
+            <div class="introjs-tooltipReferenceLayer"></div>
+            <div class="introjs-tooltip introjs-floating">
+                <div class="introjs-tooltipheader">
+                    <div class="introjs-tooltiptitle"></div>
+                </div>
+                <div class="introjs-tooltiptext">
+                    <div>{"Text: "}{&config.steps[*current_step].content}</div>
+                </div>
+                <div class="introjs-bullets">
+                    <ul role="tablist">
+                        {for (0..config.steps.len()).map(|i| {
+                            let current_step = current_step.clone();
+                            let highlight_element = highlight_element.clone();
+                            let i = i as usize;
+                            let step = &config.steps[i];
+                            let is_current = *current_step == i;
+                            let on_click = Callback::from(move |_| {
+                                current_step.set(i);
+                                highlight_element.emit(());
+                            });
+
+                            html! {
+                                <li role="presentation">
+                                    <a role="button" data-step-number={format!("{}", i)} onclick={on_click} class={if is_current { "active" } else { "" }} >
+                                        {" "}
+                                    </a>
+                                </li>
+                            }
+                        })}
+                    </ul>
+                </div>
+                <div class="introjs-tooltipbuttons">
+                    <button onclick={on_prev}>{"Prev"}</button>
+                    <button onclick={on_next}>{"Next"}</button>
+                </div>
+                <div class="introjs-tooltipfooter"></div>
+            </div>
+        </>
     }
 }
 
-fn clear_highlight() {
+fn clear_highlight(selector: &str) {
     let document = web_sys::window().unwrap().document().unwrap();
-    let elements = document.query_selector_all("*").unwrap();
+    let elements = document.query_selector_all(".introjs-helperNumberLayer").unwrap();
     for i in 0..elements.length() {
         if let Some(node) = elements.item(i) {
             if let Ok(element) = node.dyn_into::<Element>() {
                 element.set_attribute("style", "").unwrap();
+                element.set_attribute("class", selector).unwrap();
             }
         }
     }
