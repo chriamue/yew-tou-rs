dba9c4dfd1a28715cd8b44650efed7ba2cc41a02
Christian M <chriamue@gmail.com>
2024-06-23 09:01:58 +0200

    :lipstick: add styles for example

--- examples/basic.rs
+++ examples/basic.rs
@@ -15,16 +15,23 @@ pub fn app() -> Html {
         TourStep {
             selector: ".button-step".to_string(),
             content: "This is a button".to_string(),
+        },
+        TourStep {
+            selector: "footer".to_string(),
+            content: "This is a footer".to_string(),
         }
     ];
 
     html! {
-        <>
+        <div class="app">
             <h1 class="h1-step" data-title="h1" data-intro="This is a title">{"Hello, Yew Tou-rs"}</h1>
             <p class="p-step" data-title="p" data-intro="This is a paragraph">{"This is a simple example of a Yew Tou-rs."}</p>
             <button class="button-step">{"Click me"}</button>
+            <footer class="footer-step">
+                <a href="https://github.com/chriamue/yew-tou-rs">{"yew-tou-rs"}</a>
+            </footer>
             <Tour steps={steps} selector={"introduction"}/>
-        </>
+        </div>
     }
 }
 
