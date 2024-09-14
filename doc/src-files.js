89d518ae53e61d538496443d28e80070281db183
chriamue <chriamue@users.noreply.github.com>
2024-09-14 08:38:16 +0000

    deploy: 0a0753654ece24eb328920ec171bf32c057fecb5

--- doc/src-files.js
+++ doc/src-files.js
@@ -1,5 +1,5 @@
 var srcIndex = new Map(JSON.parse('[\
 ["example_basic",["",[],["basic.rs"]]],\
-["yew_tou_rs",["",[],["config.rs","content.rs","lib.rs","navigation.rs","position.rs","progress.rs","rect.rs","relative_position.rs","selection.rs","step.rs","step_info.rs","tour.rs"]]]\
+["yew_tou_rs",["",[["components",[],["content.rs","mod.rs","navigation.rs","progress.rs","selection.rs","step_info.rs"]],["models",[],["config.rs","mod.rs","rect.rs","step.rs"]],["utils",[],["mod.rs","position.rs","relative_position.rs"]]],["lib.rs","tour.rs"]]]\
 ]'));
 createSrcSidebar();
